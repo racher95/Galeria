519f078b1d14220e1a53038625125f96952a2e9e
Bryan Luzardo <tolovehayley@gmail.com>
2025-08-08 11:26:25 -0300

    Continuamos con modularizacion

--- script.js
+++ script.js
@@ -1,49 +1,11 @@
  // Datos de las imágenes
 import { images } from './imagenes.js'   
 import { crearGaleria } from './dom.js'
+import { openModal, changeSlide, closeModal } from './modal.js'
 
-let currentImageIndex = 0;
 
-// Abrir modal con imagen específica
-function openModal(index) {
-    currentImageIndex = index;
-    updateModalContent();
-    document.getElementById("imageModal").style.display = "block";
-    document.body.style.overflow = "hidden"; // Prevenir scroll del body
-}
 
-// Cerrar modal
-function closeModal() {
-    document.getElementById("imageModal").style.display = "none";
-    document.body.style.overflow = "auto";
-}
 
-// Cambiar imagen en el modal
-function changeSlide(direction) {
-    currentImageIndex += direction;
-
-    // Navegación circular
-    if (currentImageIndex >= images.length) {
-        currentImageIndex = 0;
-    } else if (currentImageIndex < 0) {
-        currentImageIndex = images.length - 1;
-    }
-
-    updateModalContent();
-}
-
-      // Actualizar contenido del modal
-function updateModalContent() {
-    const image = images[currentImageIndex];
-    document.getElementById("modalImage").src = image.src;
-    document.getElementById("modalImage").alt = image.alt;
-    document.getElementById("modalTitle").textContent = image.title;
-    document.getElementById("modalDescription").textContent =
-        image.description;
-    document.getElementById("currentSlide").textContent =
-        currentImageIndex + 1;
-    document.getElementById("totalSlides").textContent = images.length;
-}
 
 // Navegación con teclado
 document.addEventListener("keydown", function (event) {
