dd756e61de97205151f1ffa8d83780279b8f357e
kevin <kevincamara95@gmail.com>
2025-08-08 10:50:58 -0300

    Resueltos conflictos de fusión entre main y Bryan

---
+++ imagenes.js
@@ -0,0 +1,17 @@
+export const images = [
+        {
+          src: "img1.jpg",
+          alt: "Imagen de la NASA",
+          title: "Tecnología Espacial",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur minus quia fugiat quae cumque consequatur suscipit quidem? Cupiditate, quos debitis recusandae sequi, fugit quis nihil omnis atque inventore consequatur soluta?",
+        },
+        {
+          src: "img2.jpg",
+          alt: "Sala de servidores",
+          title: "Infraestructura Digital",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur minus quia fugiat quae cumque consequatur suscipit quidem? Cupiditate, quos debitis recusandae sequi, fugit quis nihil omnis atque inventore consequatur soluta?",
+        },
+        
+];
