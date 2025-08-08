8714faedc23e952680f62c90cb067270faa48fbf
kevin <kevincamara95@gmail.com>
2025-08-08 11:41:28 -0300

    se crea carpeta de imagenes

--- imagenes.js
+++ imagenes.js
@@ -1,27 +1,27 @@
 export const images = [
         {
-          src: "img1.jpg",
+          src: "./Img/img1.jpg",
           alt: "Imagen de la NASA",
           title: "Tecnología Espacial",
           description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur minus quia fugiat quae cumque consequatur suscipit quidem? Cupiditate, quos debitis recusandae sequi, fugit quis nihil omnis atque inventore consequatur soluta?",
         },
         {
-          src: "img2.jpg",
+          src: "./Img/img2.jpg",
           alt: "Sala de servidores",
           title: "Infraestructura Digital",
           description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur minus quia fugiat quae cumque consequatur suscipit quidem? Cupiditate, quos debitis recusandae sequi, fugit quis nihil omnis atque inventore consequatur soluta?",
         },
         {
-          src: "img3.jpg",
+          src: "./Img/img3.jpg",
           alt: "Robot humanoide",
           title: "Inteligencia Artificial",
           description:
             "La inteligencia artificial (IA) es la tecnología que permite a las máquinas realizar tareas que requieren inteligencia humana, como aprender, razonar y reconocer patrones, usando datos y algoritmos para mejorar con el tiempo.",
         },
         {
-          src: "img4.jpg",
+          src: "./Img/img4.jpg",
           alt: "CiberSeguridad",
           title: "CiberSeguridad",
           description:
